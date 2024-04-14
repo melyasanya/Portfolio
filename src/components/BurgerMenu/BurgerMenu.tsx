14f58e48d46aabb1881b22c84b3dbd4794f745f3
melyasanya <werbolom@gmail.com>
2024-04-14 15:42:56 +0300

    animated navigation menu

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,4 +1,5 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { Navigation } from "../Navigation/Navigation";
 import { useState } from "react";
 
 export const BurgerMenu = () => {
@@ -7,34 +8,60 @@ export const BurgerMenu = () => {
   const toggleMenu = () => setActive(!active);
 
   return (
-    <div>
-      <motion.button
-        onClick={toggleMenu}
-        animate={active ? "open" : "closed"}
-        className="flex flex-col space-y-1"
-      >
-        <motion.span
-          variants={{
-            closed: { rotate: 0, y: 0 },
-            open: { rotate: 45, y: 6 },
-          }}
-          className="w-5 h-[2px] bg-black block"
-        ></motion.span>
-        <motion.span
-          variants={{
-            closed: { opacity: 1 },
-            open: { opacity: 0 },
-          }}
-          className="w-5 h-[2px] bg-black block"
-        ></motion.span>
-        <motion.span
-          variants={{
-            closed: { rotate: 0, y: 0 },
-            open: { rotate: -45, y: -6 },
-          }}
-          className="w-5 h-[2px] bg-black block"
-        ></motion.span>
-      </motion.button>
-    </div>
+    <>
+      <div className="z-10">
+        <motion.button
+          onClick={toggleMenu}
+          animate={active ? "open" : "closed"}
+          className="flex flex-col space-y-1"
+        >
+          <motion.span
+            variants={{
+              closed: { rotate: 0, y: 0 },
+              open: { rotate: 45, y: 6 },
+            }}
+            className="w-5 h-[2px] bg-black block"
+          ></motion.span>
+          <motion.span
+            variants={{
+              closed: { opacity: 1 },
+              open: { opacity: 0 },
+            }}
+            className="w-5 h-[2px] bg-black block"
+          ></motion.span>
+          <motion.span
+            variants={{
+              closed: { rotate: 0, y: 0 },
+              open: { rotate: -45, y: -6 },
+            }}
+            className="w-5 h-[2px] bg-black block"
+          ></motion.span>
+        </motion.button>
+      </div>
+      <AnimatePresence>
+        {active && (
+          <motion.div
+            initial={{ scaleY: 0 }}
+            animate={{
+              scaleY: 1,
+              transition: {
+                duration: 0.2,
+                ease: [0.12, 0, 0.39, 0],
+              },
+            }}
+            exit={{
+              scaleY: 0,
+              transition: {
+                duration: 0.5,
+                ease: [0.22, 1, 0.36, 1],
+              },
+            }}
+            className="bg-white absolute left-0 top-[63px] w-full h-[420px] origin-top border-b-[10px] border-black"
+          >
+            <Navigation />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
