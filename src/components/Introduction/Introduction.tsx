d124cf5ba4d6bba149df0361a9f95459b52eeeba
melyasanya <werbolom@gmail.com>
2024-04-16 20:49:11 +0300

    fix photo bug

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -1,8 +1,10 @@
+import MyPhoto from "../../assets/images/avatar.jpg";
+
 export const Introduction = () => {
   return (
     <div className="flex gap-[8px]">
       <img
-        src="/src/assets/images/avatar.JPG"
+        src={MyPhoto}
         alt="My photo"
         className="w-[48px] h-[48px] rounded border"
       />
