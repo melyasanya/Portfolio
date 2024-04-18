eeb47189d73eeaeb3901c2d5e2acd8bd602cc4dc
melyasanya <werbolom@gmail.com>
2024-04-18 20:51:17 +0300

    added env

--- src/utils/sendMessage.ts
+++ src/utils/sendMessage.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const telegramApiKey = import.meta.env.VITE_TELEGRAM_API_KEY;
+const telegramChatId = import.meta.env.VITE_TELEGRAM_CHAT_ID;
+
 export const sendMessage = (name: string, email: string, message: string) => {
-  const url = `https://api.telegram.org/bot6592581516:AAFr2e78-NUfpwSwudmIpzEQKJp_aliQHPw/sendMessage?chat_id=399217561`;
+  const url = `https://api.telegram.org/bot${telegramApiKey}/sendMessage?chat_id=${telegramChatId}`;
   const data = axios.post(url, {
     parse_mode: "html",
     text: `You got a message from ${email}, name is ${name} with a following text: ${message}`,
