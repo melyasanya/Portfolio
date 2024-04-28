ec449488ca116a3815858e23ce90a1da25209454
melyasanya <werbolom@gmail.com>
2024-04-28 12:30:21 +0300

    check

--- src/components/DownloadBtn/DownloadBtn.tsx
+++ src/components/DownloadBtn/DownloadBtn.tsx
@@ -4,7 +4,7 @@ import { GoDownload } from "react-icons/go";
 export const DownloadBtn = () => {
   return (
     <Link
-      to="../../../public/files/Oleksandr_Melnychenko_CV.pdf"
+      to="/files/Oleksandr_Melnychenko_CV.pdf"
       target="_blank"
       download
       className="flex justify-center items-center gap-[6px] text-text p-small border border-black rounded active:bg-activeLink md:w-[200px] md:px-0"
